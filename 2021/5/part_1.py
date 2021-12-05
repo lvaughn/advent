879830c46f8b56c80d833f66d3a3f4f510423858
Lowell Vaughn <lowell@vaughnresearch.com>
2021-12-04 21:26:06 -0800

    2021-12-04

---
+++ 2021/5/part_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import re
+
+import_re = re.compile(r'(\d+),(\d+)\D*(\d+),(\d+)')
+
+line_segments = []
+with open("input.txt", "r") as infile:
+    for line in infile:
+        m = import_re.match(line)
+        segment = ((int(m[1]), int(m[2])), (int(m[3]), int(m[4])))
+        line_segments.append(segment)
+
+board = np.zeros((1000, 1000), dtype=int)
+for segment in line_segments:
+    ((x1, y1), (x2, y2)) = segment
+    if x1 == x2:
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            board[x1, y] += 1
+    elif y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            board[x, y1] += 1
+
+n_overlap = (board > 1).sum()
+print(n_overlap)
