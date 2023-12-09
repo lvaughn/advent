8caeef2c0bdeebab195bb30f19b65043e1282c8b
Lowell Vaughn <lowell@vaughnresearch.com>
2023-12-08 17:47:29 -0800

    Redux

---
+++ 2019_redux/1/part_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#from string import ascii_uppercase, ascii_lowercase, digits
+#from collections import Counter, defaultdict, deque, namedtuple
+#from itertools import count, product, permutations, combinations, combinations_with_replacement
+#from sortedcontainers import SortedSet, SortedDict, SortedList
+#import numpy as np
+#import re
+#import pprint
+import sys
+
+
+# Itertools Functions:
+# product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
+# permutations('ABCD', 2)                     AB AC AD BA BC BD CA CB CD DA DB DC
+# combinations('ABCD', 2)                     AB AC AD BC BD CD
+# combinations_with_replacement('ABCD', 2)    AA AB AC AD BB BC BD CC CD DD
+
+with open(sys.argv[1], 'r') as infile:
+    lines = [l.strip() for l in infile]
+answer = sum((int(l)//3) - 2 for l in lines)
+
+print("Part 1", answer)
+
+new_fuel = (answer//3) - 2
+while new_fuel > 0:
+    answer += new_fuel
+    new_fuel = (new_fuel//3) - 2
+    
+print(f"Part 2: {answer}")
