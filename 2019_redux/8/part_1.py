40228f96c35a862a296631bd28b8b197fab6c112
Lowell Vaughn <lowell@vaughnresearch.com>
2023-12-09 20:09:33 -0800

    Redux

--- 2019_redux/8/part_1.py
+++ 2019_redux/8/part_1.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-#from string import ascii_uppercase, ascii_lowercase, digits
-#from collections import Counter, defaultdict, deque, namedtuple
-#from itertools import count, product, permutations, combinations, combinations_with_replacement
-#from sortedcontainers import SortedSet, SortedDict, SortedList
+
 import numpy as np
-#import re
-#import pprint
 import sys
 
-
-# Itertools Functions:
-# product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
-# permutations('ABCD', 2)                     AB AC AD BA BC BD CA CB CD DA DB DC
-# combinations('ABCD', 2)                     AB AC AD BC BD CD
-# combinations_with_replacement('ABCD', 2)    AA AB AC AD BB BC BD CC CD DD
 
 with open(sys.argv[1], 'r') as infile:
     lines = [l.strip() for l in infile]
@@ -22,8 +11,6 @@ answer = 0
 n_numbers = len(lines[0])
 width = 25
 height = 6
-# width = 3
-# height = 2
 depth = n_numbers // (width * height)
 assert n_numbers % (width * height) == 0
 
