148325cd3c8b149a78ab14e938ade2bde10cfdd0
Lowell Vaughn <lowell@vaughnresearch.com>
2025-12-05 21:36:36 -0800

    Day 6

--- 2025/6/part2.py
+++ 2025/6/part2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 import sys
+#import numpy as np
+import re
 # from collections import Counter, defaultdict, deque, namedtuple
 # from string import ascii_uppercase, ascii_lowercase, digits
 # from itertools import count, product, permutations, combinations, combinations_with_replacement
 # from sortedcontainers import SortedSet, SortedDict, SortedList
 # from z3 import Solver, BitVec, Distinct
 # from networkx import networkx as nx  
-# import sympy as sym
 # from intervaltree import IntervalTree
-
 # import pprint
+# import sympy as sym
 # from functools import cache
-# import numpy as np
-# import re
+ 
 
 # Itertools Functions:
 # product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
@@ -29,5 +29,46 @@ with open(sys.argv[1], 'r') as infile:
 height = len(lines)
 width = len(lines[0])
 
+def all_spaces(loc):
+    for l in lines[:-1]:
+        if l[loc] != ' ':
+            return False 
+    return True 
 
-print("Part 1", answer)
+def get_number(loc):
+    val = ''
+    for l in lines[:-1]:
+        if l[loc] != ' ':
+            val += l[loc]
+    return int(val)
+
+def get_result(op, nums):
+    if op == '+':
+        return sum(nums)
+    else:
+        assert op == '*'
+        val = 1
+        for n in nums:
+            val *= n 
+        return val 
+            
+op = None
+numbers = []
+for i in range(width):
+    # print(i, ch)
+    if i >= len(lines[-1]):
+        ch = ' ' # We're off the end of the array
+    else: 
+        ch = lines[-1][i]
+    if all_spaces(i):
+        assert ch == ' '
+        answer += get_result(op, numbers) 
+    elif ch != ' ':
+        op = ch 
+        numbers = [get_number(i)]
+    else:
+        numbers.append(get_number(i))
+        
+answer += get_result(op, numbers) 
+
+print("Part 2:", answer)
--- 2025/6/part_1.py
+++ 2025/6/part_1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 import sys
+#import numpy as np
+import re
 # from collections import Counter, defaultdict, deque, namedtuple
 # from string import ascii_uppercase, ascii_lowercase, digits
 # from itertools import count, product, permutations, combinations, combinations_with_replacement
 # from sortedcontainers import SortedSet, SortedDict, SortedList
 # from z3 import Solver, BitVec, Distinct
 # from networkx import networkx as nx  
-# import sympy as sym
 # from intervaltree import IntervalTree
-
 # import pprint
+# import sympy as sym
 # from functools import cache
-# import numpy as np
-# import re
+
 
 # Itertools Functions:
 # product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
@@ -29,5 +29,17 @@ with open(sys.argv[1], 'r') as infile:
 height = len(lines)
 width = len(lines[0])
 
+
+parsed_lines = [l.split() for l in lines]
+for i, op in enumerate(parsed_lines[4]):
+    if op == '+':
+        val = sum(int(parsed_lines[n][i]) for n in range(4))
+        answer += val 
+    else: 
+        val = 1
+        for n in range(4):
+            val *= int(parsed_lines[n][i])
+        answer += val  
+    
 
 print("Part 1", answer)
